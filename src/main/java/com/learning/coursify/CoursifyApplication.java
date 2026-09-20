d52586ed6fe5b4adbb4b7ab66c3f2acac870e295
kunal suner <kunalsuner@gmai.com>
2026-09-20 23:42:26 +0530

    git initial commit-basic structure

---
+++ src/main/java/com/learning/coursify/CoursifyApplication.java
@@ -0,0 +1,15 @@
+package com.learning.coursify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursifyApplication.class, args);
+
+
+		System.out.println("Application start");
+	}
+}
